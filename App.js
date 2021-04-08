4b4d39f8bfcf51bfdd99331f77ea3ba41069e343
bmurdock <brian.j.murdock@gmail.com>
2021-04-07 19:54:28 -0600

    Wednesday April 7

--- App.js
+++ App.js
@@ -1,7 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, Image} from 'react-native';
+import { StyleSheet, Text, View, Image, Button, Picker} from 'react-native';
 import { rando, pokeFetch } from './util';
+import {
+  Directions,
+  State,
+  FlingGestureHandler,
+} from 'react-native-gesture-handler';
 
 const maxPoke = 898;
 
@@ -12,40 +17,117 @@ export default class App extends Component {
     this.state = {
         pokemonName: 'Brian',
         pokemonImage: { uri: ''},
+        pickerVal: 'test2',
+        toggle: true,
+        types: [],
     };
   }
 
-  async componentDidMount()
+  componentDidMount()
   {
-      const pokemon = await pokeFetch('pokemon', rando(1, maxPoke));
-      console.log('pokemon: ', pokemon);
-
-
-      // set the state property pokemonName to the
-      // name property of the pokemon that got returned
-      // from my fetch
-      this.setState({
-        pokemonName: pokemon.name,
-        pokemonImage: {uri: pokemon.sprites.front_default}
-      });
+      this.updatePokemon();
   }
 
+  // get a new pokemon (refresh pokemon that is loaded)
+  updatePokemon = async () =>
+  {
+    const pokemon = await pokeFetch('pokemon', rando(1, maxPoke));
+    this.setState({
+      pokemonName: pokemon.name,
+      pokemonImage: {uri: pokemon.sprites.front_default},
+      types: pokemon.types.map((type, i) =>
+      {
+        return <Text key={`type_${i}`}>{type.type.name}</Text>
+      }),
+    });
+  }
+  pickerHandler = (val) =>
+  {
+    // right now we just want to simply change the value
+    // to match the user selection
+    this.setState({
+      pickerVal: val,
+    });
+  }
+
+  handleSwipe = ({nativeEvent}) =>
+  {
+    console.log('nativeEvent: ', nativeEvent);
+    if (nativeEvent.state === State.END)
+    {
+      const {translationX} = nativeEvent;
+      let direction = (translationX < 0) ? "left" : "right";
+      console.log('direction: ', direction);
+      this.updatePokemon();
+      this.setState({
+        toggle: (this.state.toggle) ? false : true,
+      });
+    }
+  }
   render()
   {
-    console.log(pokeFetch('pokemon', 1));
     return (
-      <View style={styles.container}>
-          <Image
-            style={styles.pokemonImage} 
-            source={this.state.pokemonImage}
-          />
-          <Text>Name: {this.state.pokemonName}</Text>
-          <View>
-            <Text>Types {Date.now()}</Text>
-            <Text>Electric</Text>
-            <Text>Grass</Text>
-          </View>
-      </View>
+
+
+        <View style={styles.container}>
+            <View style={styles.menu}>
+            <Button
+              onPress={() => { console.log('works')}}
+              title="Home"
+              color="#000"
+              disabled={this.state.toggle}
+              />
+                          <Button
+              onPress={() => { console.log('works')}}
+              title="Home"
+              color="#000"
+              disabled={true}
+              />
+                          <Button
+              onPress={() => { console.log('works')}}
+              title="Home"
+              color="#000"
+              />
+            </View>
+            <FlingGestureHandler
+              direction={Directions.LEFT | Directions.RIGHT}
+              onGestureEvent={this.handleSwipe}
+            >
+              <Image
+                style={styles.pokemonImage} 
+                source={this.state.pokemonImage}
+              />
+            </FlingGestureHandler>
+            <Picker
+              selectedValue={this.state.pickerVal}
+              onValueChange={this.pickerHandler}
+            
+            >
+              <Picker.Item
+                  label="Test 1"
+                  value="test1"
+                  />
+              <Picker.Item
+                label="Test 2"
+                value="test2"
+                />
+              <Picker.Item
+                label="Test 3"
+                value="test3"
+                />
+
+            </Picker>
+            <Text>Name: {this.state.pokemonName}</Text>
+            <Text>Something like this.</Text>
+            <View>
+              {this.state.types}
+            </View>
+            <Button
+              title="Change"
+              onPress={this.updatePokemon}
+            />
+        </View>
+
     );
   }
 
@@ -66,6 +148,18 @@ const styles = StyleSheet.create({
     borderColor: 'black',
     width: 200,
     height: 200,
-  }, 
+  },
+  menu: {
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-evenly',
+  },
+  menuItem: {
+    borderWidth: 1,
+    borderColor: 'black',
+    padding: 5,
+    margin: 1,
+  }
 
 });
